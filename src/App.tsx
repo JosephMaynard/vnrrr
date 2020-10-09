ac8f842a31597f70a312a7849ea6482110ace9a3
Joseph Maynard <joseph.maynard@gmail.com>
2020-10-09 20:47:50 +0100

    Add Posts to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
 
-import './App.css';
+import Posts from './components/Posts/Posts';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Posts />
     </Provider>
   );
 }
