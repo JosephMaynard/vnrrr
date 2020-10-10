ce3c37d9e6647f94ce563d1c43338999c37d95a1
Joseph Maynard <joseph.maynard@gmail.com>
2020-10-10 15:13:54 +0100

    Move Provider to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-
-import store from './store';
 
 import Posts from './components/Posts/Posts';
 
 const App: React.FC = (): JSX.Element => {
-  return (
-    <Provider store={store}>
-      <Posts />
-    </Provider>
-  );
+  return <Posts />;
 };
 
 export default App;
