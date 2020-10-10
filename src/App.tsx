738d2ea6af5ab4f202d3c3d8e812d81abae8b104
Joseph Maynard <joseph.maynard@gmail.com>
2020-10-10 15:32:16 +0100

    Add routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Posts from './components/Posts/Posts';
 
 const App: React.FC = (): JSX.Element => {
-  return <Posts />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/r/:subreddit">
+          <Posts />
+        </Route>
+        <Redirect to="/r/all" />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
