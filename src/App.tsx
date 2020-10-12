eca069150d1e265823bf3ca30b2c721fac1f0236
Joseph Maynard <joseph.maynard@gmail.com>
2020-10-12 18:03:19 +0100

    Add Logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Posts from './components/Posts/Posts';
 
+import Logo from './components/Logo/Logo';
+
 const App: React.FC = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/r/:subreddit">
           <Posts />
+        </Route>
+        <Route path="/logo">
+          <div className="Logo_previewWrapper">
+            <Logo />
+          </div>
         </Route>
         <Redirect to="/r/all" />
       </Switch>
