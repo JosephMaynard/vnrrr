271826af77e1b38d83d9b6e85cbecdc453a7b74d
Joseph Maynard <joseph.maynard@gmail.com>
2020-10-24 16:51:25 +0100

    Use OutsideClickHandler in SideMenu

--- src/components/Layout/SideMenu.tsx
+++ src/components/Layout/SideMenu.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import OutsideClickHandler from 'react-outside-click-handler';
+
 import IconButton from '../IconButton/IconButton';
 
 export interface IProps {
@@ -10,19 +12,21 @@ export type TRef = HTMLDivElement;
 
 const SideMenu = React.forwardRef<TRef, IProps>(
   ({ close }: IProps, ref): JSX.Element => (
-    <div ref={ref} className="SideMenu">
-      <IconButton
-        icon="cancel"
-        onClick={close}
-        className="SideMenu_closeButton"
-      />
-      <Link to="/" onClick={close} className="SideMenu_link">
-        Frontpage
-      </Link>
-      <Link to="/r/all" onClick={close} className="SideMenu_link">
-        /r/all
-      </Link>
-    </div>
+    <OutsideClickHandler onOutsideClick={close}>
+      <div ref={ref} className="SideMenu">
+        <IconButton
+          icon="cancel"
+          onClick={close}
+          className="SideMenu_closeButton"
+        />
+        <Link to="/" onClick={close} className="SideMenu_link">
+          Frontpage
+        </Link>
+        <Link to="/r/all" onClick={close} className="SideMenu_link">
+          /r/all
+        </Link>
+      </div>
+    </OutsideClickHandler>
   )
 );
 
