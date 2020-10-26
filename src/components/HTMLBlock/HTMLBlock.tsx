854160bb5f636fb0db2825a74b3a6acfeb19e404
Joseph Maynard <joseph.maynard@gmail.com>
2020-10-26 17:52:01 +0000

    Use sanitise-html to update A tag attributes

--- src/components/HTMLBlock/HTMLBlock.tsx
+++ src/components/HTMLBlock/HTMLBlock.tsx
@@ -12,7 +12,48 @@ const HTMLBlock: React.FC<IProps> = ({
   html,
   className,
 }: IProps): JSX.Element => {
-  const cleanHtml = useMemo(() => ({ __html: sanitizeHtml(html) }), [html]);
+  const cleanHtml = useMemo(
+    () => ({
+      __html: sanitizeHtml(html, {
+        transformTags: {
+          a: (tagName: string, attribs: { href: string }) => {
+            if (attribs.href.substr(0, 1) === '/') {
+              return {
+                tagName: 'a',
+                attribs: {
+                  ...attribs,
+                },
+              };
+            } else if (attribs.href.substr(0, 21) === 'https://reddit.com/r/') {
+              return {
+                tagName: 'a',
+                attribs: {
+                  href: attribs.href.substr(18),
+                },
+              };
+            } else if (
+              attribs.href.substr(0, 25) === 'https://www.reddit.com/r/'
+            ) {
+              return {
+                tagName: 'a',
+                attribs: {
+                  href: attribs.href.substr(22),
+                },
+              };
+            }
+            return {
+              tagName: 'a',
+              attribs: {
+                ...attribs,
+                target: '_blank',
+              },
+            };
+          },
+        },
+      }),
+    }),
+    [html]
+  );
   return (
     <div
       className={`HTMLBlock${className ? ` ${className}` : ''}`}
